c9e6e1e1783ead311229db39013cb41936677e89
GComputeNerd <GComputeNerd@users.noreply.github.com>
2024-01-23 10:14:57 +0530

    Added Initial Shortcut Gen Code

--- shortcut_gen.py
+++ shortcut_gen.py
@@ -1,1 +1,19 @@
 # This python script generates shortcuts for this rainmeter skin
+
+header = """========================================================================
+  ____  ____                            _       _   _              _ 
+ / ___|/ ___|___  _ __ ___  _ __  _   _| |_ ___| \ | | ___ _ __ __| |
+| |  _| |   / _ \| '_ ` _ \| '_ \| | | | __/ _ \  \| |/ _ \ '__/ _` |
+| |_| | |__| (_) | | | | | | |_) | |_| | ||  __/ |\  |  __/ | | (_| |
+ \____|\____\___/|_| |_| |_| .__/ \__,_|\__\___|_| \_|\___|_|  \__,_|
+                           |_|                                       
+                        OneLauncher Shortcut Gen
+                        
+                        Author : Afras Ashraf
+                        Github : @GComputeNerd
+========================================================================
+"""
+
+print(header)
+menu = input("Which menu do you want to save shortcuts to? ")
+print("Saving to", menu)
